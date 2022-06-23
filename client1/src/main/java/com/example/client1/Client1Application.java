2977d3446480954d8afad8571347d8d56edc77d0
swingforme <swingforme@126.com>
2022-06-23 08:59:04 +0800

    this demo show you how to config a spring config center, that is based on local file way.

--- client1/src/main/java/com/example/client1/Client1Application.java
+++ client1/src/main/java/com/example/client1/Client1Application.java
@@ -1,13 +1,27 @@
 package com.example.client1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
+@RestController
+@RequestMapping("/test")
 @SpringBootApplication
 public class Client1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Client1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Client1Application.class, args);
+	}
 
+	@Value("${ab.bc.name:config server not found!}")
+	private String bar;
+
+	@RequestMapping("/app")
+	String hello() {
+		return "Hello " + bar + "!";
+	}
 }
